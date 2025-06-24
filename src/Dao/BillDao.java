c5a0429ddd10c975274b0a57c70740316d684559
salina12305 <prakritistha06@gmail.com>
2025-06-24 12:49:07 +0545

    added

--- src/Dao/BillDao.java
+++ src/Dao/BillDao.java
@@ -28,37 +28,7 @@ public class BillDao {
         }
     }
 
-//    public List<BillModel> getBillsByCustomerName(String customerName) {
-//        List<BillModel> billList = new ArrayList<>();
-//        Connection conn = mysql.openConnection();
-//
-//        String sql = "SELECT * FROM bills WHERE customer_name = ?";
-//        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-//            pstmt.setString(1, customerName);
-//            ResultSet rs = pstmt.executeQuery();
-//
-//            while (rs.next()) {
-//                BillModel bill = new BillModel(
-//                    rs.getInt("id"),
-//                    rs.getString("item"),
-//                    rs.getInt("quantity"),
-//                    rs.getDouble("rate"),
-//                    rs.getDouble("amount"),
-//                    rs.getDouble("discount"),
-//                    rs.getDouble("tax"),
-//                    rs.getDouble("total"),
-//                    rs.getString("customer_name")
-//                );
-//                billList.add(bill);
-//            }
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return billList;
-//    }
-    
+ 
     public List<BillModel> getBillsByCustomerName(String customerName) {
     List<BillModel> billList = new ArrayList<>();
     Connection conn = mysql.openConnection();
